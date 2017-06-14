4570892362fb119f6f64754f79b49c8e64f6f663
Shekar Singh <shekarsingh.coder15@gmail.com>
2017-06-14 21:58:31 +0530

    Add files via upload

---
+++ www/js/app.js
@@ -0,0 +1,71 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html'
+  })
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.attendance', {
+    url: '/attendance',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/attendance.html'
+      }
+    }
+  })
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+  .state('app.playlists', {
+    url: '/playlists',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'AppCtrl'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/login');
+});
+
+app.controller('AppCtrl', ['$scope', function($scope) {
+  
+}])
